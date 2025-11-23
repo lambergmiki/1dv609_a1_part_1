6f0d986b8fc5ecee60bb4e43067e1fa88b3d5664
Miki Lamberg <ml227cr@student.lnu.se>
2025-11-23 11:22:06 +0100

    Add tests for part 1 suite that pass on correct and fail on respective bug version

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,25 +1,76 @@
-
 // Select one of the Password versions to test
 
-import { Password } from '../src/BugDoesNotHash'
-// import { Password } from '../src/BugDoesNotTrim'
-// import { Password } from '../src/BugisPasswordAlwaysSame'
-// import { Password } from '../src/BugMissingNumberCheck'
-// import { Password } from '../src/BugMissingPasswordCheck'
-// import { Password } from '../src/BugNeverContainsNumbers'
-// import { Password } from '../src/BugToShortPassword'
-// import { Password } from '../src/BugVeryShort'
-// import { Password } from '../src/BugWrongHashingAlgorithm'
-// import { Password } from '../src/BugWrongMessage'
-// import { Password } from '../src/Correct'
+// import { Password } from "../src/BugDoesNotHash";
+// import { Password } from "../src/BugDoesNotTrim";
+// import { Password } from "../src/BugisPasswordAlwaysSame";
+// import { Password } from "../src/BugMissingNumberCheck";
+// import { Password } from "../src/BugMissingPasswordCheck";
+// import { Password } from "../src/BugNeverContainsNumbers";
+// import { Password } from "../src/BugToShortPassword";
+// import { Password } from "../src/BugVeryShort";
+// import { Password } from "../src/BugWrongHashingAlgorithm";
+// import { Password } from "../src/BugWrongMessage";
+import { Password } from "../src/Correct";
 
-describe('Password class, test suite', () => {
-    //put constants here to increase readability
-    const emptyPassword = '';
+describe("Password class, test suite", () => {
+	// BugDoesNotHash
+	test("getPasswordHash() should return hashed password for entered password", () => {
+		const pw = new Password("unhashedpassword123");
+		const hashedPw = pw.getPasswordHash();
+		expect(hashedPw).not.toBe("unhashedpassword123");
+	});
 
-    test('replace this test with one of your own and add more', () => {
-        expect(true).toBe(true);
-    });
+	// BugDoesNotTrim
+	test("Constructor of Password class should not trim white space", () => {
+		const pw = new Password(" hehelookatthispassword123  ");
+		const pw2 = new Password("hehelookatthispassword123");
+		expect(pw.getPasswordHash()).toBe(pw2.getPasswordHash());
+	});
 
-    //Add your tests here
+	// BugisPasswordAlwaysSame
+	test("isPasswordSame() should return false different passwords", () => {
+		const pw = new Password("samePasswordMaybe123");
+		const pw2 = new Password("samePasswordMaybe1234");
+		expect(pw.isPasswordSame(pw2)).toBe(false);
+	});
+
+	/*
+  Wrap in anonymous function because toThrow() only works on functions —
+  it needs to call the code itself and catch errors.
+
+  Can't be assigned to a variable first, because then the constructor is already executed
+  and Jest doesn't intercept the throw.
+
+  Jest only prints details for failures by default.
+  Passing tests are just summarized (name + check mark + time).
+*/
+	// BugMissingNumberCheck
+	test("Constructor should throw Exception for passwords without a number", () => {
+		expect(() => new Password("passwordNoNumbers")).toThrow("No number found");
+	});
+
+	// BugToShortPassword - checks for a password with 11 characters
+	test("Constructor should throw Exception for passwords shorter than 12 characters", () => {
+		expect(() => new Password("password123")).toThrow("Too short password");
+	});
+
+	// BugVeryShort
+	test("constructor Should Thow Exception For Short Passwords", () => {
+		expect(() => {
+			new Password("abc1234");
+		}).toThrow("Too short password");
+	});
+
+	// BugWrongHashingAlgorithm - checks that hash number is >= 20 digits long
+	test("Hashing algortihm should produce a safe hash (>= 20 digits)", () => {
+		const pw = new Password("hashedPassword123");
+		const pw2 = new Password("hashedPassword123");
+
+		expect(pw.getPasswordHash() && pw2.getPasswordHash()).toBeGreaterThan(20);
+	});
+
+	// BugWrongMessage
+	test("Constructor should display 'Too short password' for too short input for password", () => {
+		expect(() => new Password("shortpw123")).toThrow("Too short password");
+	});
 });
